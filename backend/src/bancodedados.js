3dd605f60f9a6aab715d440834c8e92b599ffa5f
Isabela Oliveira <oliveiragiovanaisabela@gmail.com>
2022-02-12 21:34:23 -0300

    bacjend

--- backend/src/bancodedados.js
+++ backend/src/bancodedados.js
@@ -1,5 +1,5 @@
 const conexao = require("./conexao");
-
+ 
 const sequence = {
   _id: 0,
   increaseId() {
@@ -7,26 +7,9 @@ const sequence = {
   }
 }
 var items = []
-
-/*Funções que precisam ser feitas para usuários:
-- Criar usuário; feita
-- Editar usuário; feita
-- Excluir usuário; feita
-- Informar dados do usuário; feita
-- Login de usuário;
-- Adicionar cartões para usuário;
-* Listar usuário: feita, porém desnecessária
-
-Funções que precisam ser feitas para cartões de ônibus:
-- Adicionar cartão;
-- Remover cartão;
-- Adicionar crédito no cartão;
-- 
-
-
-
-*/ 
-
+ 
+ 
+ 
 function createuser(user){
     const sql = "INSERT INTO Usuario SET ?"
     conexao.query(sql,item, (erro, resultados) =>
@@ -39,9 +22,9 @@ function createuser(user){
             console.log(resultados)
             return resultados
             }
-            
+           
         })
-
+ 
 function getItem(id){
     const sql = "SELECT * FROM Item WHERE id =" + id.toString()
     conexao.query(sql, (erro,resultados) =>{
@@ -55,7 +38,7 @@ function getItem(id){
     })
     return items[id]
 }
-
+ 
 function listitems(){
     const sql = "SELECT * FROM Item"
     conexao.query(sql, (erro,resultados) =>
@@ -71,8 +54,8 @@ function listitems(){
         }
     )
 }
-
-
+ 
+ 
 function edititems(id,item){
     const sql = 'UPDATE Item SET ? Where id =' + id.toString()
     conexao.query(sql, (erro,resultados) =>
@@ -87,16 +70,16 @@ function edititems(id,item){
            }
         }
     )
-
+ 
     item[id].nome = item.nome
     item[id].cpf =item.cpf
     item[id].telefone =item.telefone
     item[id].endereco = item.endereco = item.endereco
     item[id].email = item.email = item.email
     return item[id];
-
-}   
-
+ 
+}  
+ 
 function deleteitems(id){
     const sql = " DELETE from Item Where Id =" +id
     conexao.query(sql, (erro,resultados) =>
@@ -112,7 +95,7 @@ function deleteitems(id){
         }
     )
 }
-
+ 
 /*function senhaincorreta(cpf){
     const sql = " DELETE from Item Where cpf =" +cpf
     conexao.query(sql, (erro,resultados) =>
@@ -128,7 +111,7 @@ function deleteitems(id){
         }
     )
 }*/
-
+ 
 /*function login(email,senha,robo){
     const sql = "SELECT * FROM Item Where email =" +email.toString()
     conexao.querry(sql,(erro,resultados)=>
@@ -139,16 +122,17 @@ function deleteitems(id){
            }
             else{
                 if(senha==item[id].senha)
-
+ 
                console.log(resultados)
                return resultados
            }
-
+ 
     }
     )
-
+ 
 }*/
-
-
-
+ 
+ 
+ 
 module.exports = {createuser, getItem, listitems, edititems, deleteitems}}
+
